58ec6570e22933a6c288fedf5abbab7d4cd7276c
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-10 15:34:38 +0530

    scaffold: Implemented Tech Stacks Section

--- React/2 - Personal Portfolio/src/App.jsx
+++ React/2 - Personal Portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <ShowcaseSection />
+      <Carousel />
     </>
   );
 };
